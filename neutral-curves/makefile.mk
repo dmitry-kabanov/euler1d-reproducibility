444488b90785a43c9195d37789d983e067786d4b
Dmitry Kabanov <kabanovdmitry@gmail.com>
2017-11-01 20:58:52 +0300

    [neutral-curves] Add figure with two modes for gamma=1.1

--- neutral-curves/makefile.mk
+++ neutral-curves/makefile.mk
@@ -8,14 +8,21 @@ script_2 := ${exp}/plot-neutral-curves-var-gamma.py
 data_2   := ${exp}/_output/results-gamma=1.1.txt \
             ${exp}/_output/results-gamma=1.2.txt \
             ${exp}/_output/results-gamma=1.3.txt \
-            ${exp}/_output/results-gamma=1.4.txt \
+            ${exp}/_output/results-gamma=1.4.txt
+asset_3  := neutral-stability-gamma=1.1-two-modes.pdf
+script_3 := ${exp}/plot-curves-gamma\=1.1-two-modes.py
+data_3   := ${exp}/_output/results-gamma=1.1.txt \
+            ${exp}/_output/results-gamma=1.1-mode=1.txt
 
 assets_list += ${exp}/_assets/${asset_1}
 assets_list += ${exp}/_assets/${asset_2}
+assets_list += ${exp}/_assets/${asset_3}
 
 ${exp} : ${BUILD_DIR}/${asset_1}
 
 ${exp} : ${BUILD_DIR}/${asset_2}
+
+${exp} : ${BUILD_DIR}/${asset_3}
 
 ${BUILD_DIR}/${asset_1} : ${exp}/_assets/${asset_1}
 	cp $< $@
@@ -29,3 +36,8 @@ ${BUILD_DIR}/${asset_2} : ${exp}/_assets/${asset_2}
 ${exp}/_assets/${asset_2} : ${script_2} ${data_2} ${matplotlibrc_file}
 	cd ${<D} && python ${<F}
 
+${BUILD_DIR}/${asset_3} : ${exp}/_assets/${asset_3}
+	cp $< $@
+
+${exp}/_assets/${asset_3} : ${script_3} ${data_3} ${matplotlibrc_file}
+	cd ${<D} && python ${<F}
---
+++ neutral-curves/plot-curves-gamma=1.1-two-modes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from helpers import FIGSIZE_NORMAL, savefig
+
+GAMMA = 1.1
+RESULTS_FILE_MODE_0 = 'results-gamma={}.txt'.format(GAMMA)
+RESULTS_FILE_MODE_1 = 'results-gamma={}-mode=1.txt'.format(GAMMA)
+RESULTS_FILE_MODE_0 = os.path.join('_output', RESULTS_FILE_MODE_0)
+RESULTS_FILE_MODE_1 = os.path.join('_output', RESULTS_FILE_MODE_1)
+
+q_list_0, e_list_0, f_list_0 = np.loadtxt(RESULTS_FILE_MODE_0, unpack=True)
+q_list_1, e_list_1, f_list_1 = np.loadtxt(RESULTS_FILE_MODE_1, unpack=True)
+
+# Cleaning data
+idx_0 = np.where((e_list_0 != 0.0) & (e_list_0 != 140.0))[0]
+cond_1_1 = (e_list_1 != 0.0000000000000000e+00)
+cond_1_2 = (e_list_1 != 1.2517321777343751e+01)
+cond_1_3 = (e_list_1 != 1.2555725097656250e+01)
+cond_1_4 = (e_list_1 != 1.2666015625000000e+01)
+idx_1 = np.where(cond_1_1 & cond_1_2 & cond_1_3 & cond_1_4)[0]
+q_clean_0 = q_list_0[idx_0]
+e_clean_0 = e_list_0[idx_0]
+q_clean_1 = q_list_1[idx_1]
+e_clean_1 = e_list_1[idx_1]
+
+# Plot figure.
+fig = plt.figure(figsize=FIGSIZE_NORMAL)
+plt.semilogy(e_clean_0, q_clean_0, '-', label='Mode 0')
+plt.semilogy(e_clean_1, q_clean_1, '--', label='Mode 1')
+plt.hold(True)
+plt.xlim((10, 35))
+plt.ylim((2, 80))
+plt.xlabel(r'Activation energy $E$')
+plt.ylabel(r'Heat release $Q$')
+plt.grid()
+plt.tight_layout(pad=0.1)
+
+fn = 'neutral-stability-gamma={}-two-modes.pdf'.format(GAMMA)
+savefig(fn)
